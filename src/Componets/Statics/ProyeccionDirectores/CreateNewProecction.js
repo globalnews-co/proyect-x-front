9c1d5dda738b421d4471feb99e4fce1fc8e2a449
mrjhong <jfdg206@gmail.com>
2023-10-03 14:56:32 -0500

    efswe

---
+++ src/Componets/Statics/ProyeccionDirectores/CreateNewProecction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+export default function CreateNewProecction() {
+  return (
+    <div className='Body'>
+        <Navbar/>
+        <div className='container'>
+
+        </div>
+
+    </div>
+  )
+}
